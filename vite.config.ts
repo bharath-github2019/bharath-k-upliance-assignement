586233aef52f760d58785a78be51aa99f5cd32ad
Bharath Subba Reddy <56739171+bharath-github2019@users.noreply.github.com>
2025-08-10 18:08:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { webcrypto } from 'crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as unknown as Crypto;
+}
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+});
